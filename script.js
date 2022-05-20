e04f5a69157d2292fdabee9f69bf6df01bb4c931
Hoangdao192 <nguyendanghoangdao2002@gmail.com>
2022-05-20 22:40:58 +0700

    main layout and responsive

---
+++ script.js
@@ -0,0 +1,11 @@
+let openSidebarButton = document.querySelector(".open-sidebar");
+let sidebar = document.querySelector(".container__left");
+openSidebarButton.addEventListener('click', function() {   
+    sidebar.style.animation = "leftFlyIn 0.3s ease"; 
+    sidebar.style.display = "flex";
+})
+
+let closeSideBarButton = document.querySelector(".close-sidebar");
+closeSideBarButton.addEventListener('click', function(){
+    sidebar.style.animation = "leftFlyOut 0.3s ease forwards";
+})
